7a63bb4f6aacc938f45f4692d8471883a77c25b2
Telmo Trooper <telmo.trooper@gmail.com>
2017-09-17 00:36:30 -0300

    New comments

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var api = require('./routes/api');
 
+/* Initialize application */
 var app = express();
 
-
+/* Set port and start listening */
 app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-/* view engine setup */
+/* View engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -34,20 +34,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/api', api);
 
-// catch 404 and forward to error handler
+/* Catch 404 and forward to error handler */
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+/* Error handler */
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  /* Set locals, only providing error in development */
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  /* Render the error page */
   res.status(err.status || 500);
   res.render('error');
 });
