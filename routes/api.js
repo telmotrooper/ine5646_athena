21f6a8abd03cb402a46f772c0314b2c9c5d1873b
Telmo Trooper <telmo.trooper@gmail.com>
2017-09-17 17:12:50 -0300

    Biddings API now has products

--- routes/api.js
+++ routes/api.js
@@ -16,9 +16,7 @@ router.get('/', function(req, res, next) {
           res.send("Error running query.");
         } else {
           /* At this point you already have all the biddings in the 'rows' array */
-
           myJSON = rows;
-
           next(); // Run next router.get('/') function;
         }
       });
@@ -28,9 +26,43 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  
-  res.type("application/json");
-  res.send(myJSON);
+  var db = new sqlite3.Database(db_file, (error) => {
+    if(error) {
+      return console.error(error.message);
+    } else {
+      db.all("SELECT bidding, name AS product_name, product AS product_id, quantity FROM Biddings_Products JOIN Products ON product = id", function(error, rows) {
+        if(error) {
+          res.send("Error running query.");
+        } else {
+          /* Give each bidding it's own products array */
+          for(var i = 0; i < myJSON.length; i++) {
+            myJSON[i].products = [];
+          }
+
+          /* Iterate through all products in biddings */
+          for(var j = 0; j < rows.length; j++) {
+
+            /* For each product, iterate through all biddings again */
+            for(var k = 0; k < myJSON.length; k++) {
+              if(myJSON[k].id == rows[j].bidding) {
+                myJSON[k].products.push(rows[j]);
+                break;
+              }
+            }
+          }
+
+          console.log("--- Biddings ---");
+          console.log(myJSON);
+
+          console.log("--- Products ---");
+          console.log(rows);
+
+          res.type("application/json");
+          res.send(myJSON);
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
