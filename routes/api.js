539b8a3a3bb1396d7b88fa140f7e13d86b0571cf
Telmo Trooper <telmo.trooper@gmail.com>
2017-09-17 04:17:48 -0300

    First version of the API

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var sqlite3 = require('sqlite3').verbose();
+// var asyncJSON = require('async-json');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  myJSON = [];
+
+  var db = new sqlite3.Database('athena.db', (err) => {
+    if(err) {
+      return console.error(err.message);
+    } else {
+      db.all("SELECT * from Biddings", function(error, rows) {
+        for(var i = 0; i < rows.length; i++) {
+          myJSON.push(rows[i]);
+        };
+
+        res.type("application/json");
+        res.send(myJSON);
+      });
+    };
+  });
+  db.close();
 });
 
 module.exports = router;
