eeedde18d55346c9c358cd749b52669ed7c950b8
Telmo Trooper <telmo.trooper@gmail.com>
2017-09-17 00:04:39 -0300

    Added favicon

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Dependencies */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,10 +6,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+/* Pages */
 var index = require('./routes/index');
 var api = require('./routes/api');
 
 var app = express();
+
 
 app.set('port', (process.env.PORT || 8000));
 
@@ -16,12 +19,12 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-// view engine setup
+/* view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
