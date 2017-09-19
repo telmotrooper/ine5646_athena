73e8576e6a8048e358298210f5ea8f0682f426a6
Telmo "Trooper <telmo.trooper@gmail.com>
2017-09-19 18:31:04 -0300

    Preparation to add products

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var ip;
 router.get('/', function(req, res, next) {
   /* Get own IP */
   require('dns').lookup(require('os').hostname(), function (error, address) {
-    /* Request from own API */
+    /* Request biddings from own API */
     ownAPI = "http://" + address + ":" + req.app.get('port') + "/api";
 
     request(ownAPI, function(error, response, body) {
@@ -16,23 +16,35 @@ router.get('/', function(req, res, next) {
         console.log("Error: ", error);
       } else {
         parseJSON(body, function(error, biddingsJSON) {  // Get JSON object from API
-          res.render('index', {
-            biddings: biddingsJSON, // biddings from the database
-            title: 'Gerenciar licitações',
-            new_bidding: 'Nova licitação',
-            name: 'Nome',
-            applicant: 'Requerente',
-            start_date: 'Data de início',
-            end_date: 'Data de fim',
-            products: 'Produtos',
-          
-            confirm: 'Confirmar',
-            cancel: 'Cancelar',
-          
-            name_placeholder: 'Exemplo: Material para restauração da ponte',
-            applicant_placeholder: 'Exemplo: Prefeitura de Florianópolis',
-            start_date_placeholder: 'Formato: dd/mm/aaaa',
-            end_date_placeholder: 'Formato: dd/mm/aaaa'
+          /* Request products form own API */
+          request(ownAPI + "/products", function(error, response, body) {
+            if(error) {
+              console.log("Error: ", error);
+            } else {
+              parseJSON(body, function(error, productsJSON) {
+                console.log(productsJSON);
+
+                res.render('index', {
+                  biddings: biddingsJSON,  // biddings from the database
+                  products: productsJSON,  // products from the database
+                  title: 'Gerenciar licitações',
+                  new_bidding: 'Nova licitação',
+                  name: 'Nome',
+                  applicant: 'Requerente',
+                  start_date: 'Data de início',
+                  end_date: 'Data de fim',
+                  products: 'Produtos',
+                
+                  confirm: 'Confirmar',
+                  cancel: 'Cancelar',
+                
+                  name_placeholder: 'Exemplo: Material para restauração da ponte',
+                  applicant_placeholder: 'Exemplo: Prefeitura de Florianópolis',
+                  start_date_placeholder: 'Formato: dd/mm/aaaa',
+                  end_date_placeholder: 'Formato: dd/mm/aaaa'
+                });
+              });
+            }
           });
         });
       }
