94a8d61ee3f6a83193e66475ddfebcfb97450bca
Telmo "Trooper <telmo.trooper@gmail.com>
2017-09-19 19:32:59 -0300

    Products interface in the works

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,7 @@
+function change_name(name) {
+	console.log("I'm working, " + name + "!");
+}
+
 $(document).ready(function() {
 	$("#refresh-products").click(function() {
 		$.post("/refresh_products", function() {
