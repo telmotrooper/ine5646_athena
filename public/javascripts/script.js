00d4da1dca4a631260977425b58594d7ee7afc30
Telmo "Trooper <telmo.trooper@gmail.com>
2017-09-20 15:29:17 -0300

    A lot of jQuery wizardry

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,32 @@
-function change_name(name) {
-	$("#product-1").text(name);
+var baseProduct;
+var productCounter = 1;
+
+function change_name(product, name) {
+	product = $(product);  // Wrapping DOM element in a jQuery element
+
+	console.log(product.closest("div").children("button").prop("id"));
+
+	// $(product.id).text(name);
+}
+
+function new_product() {
+	// console.log(baseProduct.children("button").prop("id"));
+
+	productCounter++;
+
+	newProduct = baseProduct.clone();
+	newProduct.children("button").prop("id", "product-" + productCounter);
+	newProduct.prop("id", "dropdown-" + productCounter);
+
+	$("#products").append(newProduct);
 }
 
 $(document).ready(function() {
+	baseProduct = $(".dropdown").clone();
+
+	new_product();
+	new_product();
+
 	$("#refresh-products").click(function() {
 		$.post("/refresh_products", function() {
 			/* Reload page after refreshing products */
