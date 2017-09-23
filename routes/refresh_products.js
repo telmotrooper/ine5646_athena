4937eb14029f4917a8c55150530dcfcf32f4fd21
Telmo Trooper <telmo.trooper@gmail.com>
2017-09-23 15:35:54 -0300

    Refactoring on refresh_products.js

--- routes/refresh_products.js
+++ routes/refresh_products.js
@@ -1,51 +1,51 @@
-var express = require('express');
-var sqlite3 = require('sqlite3').verbose();
-var request = require('request');
-var parseJSON = require('json-parse-async');
-var router = express.Router();
+const express = require('express');
+const parseJSON = require('json-parse-async');
+const request = require('request');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
 
 router.post('/', function(req, res) {
-  /* Get products from API */
-  request("https://ine5646products.herokuapp.com/api/products", function(error, response, body) {
-    if(error) {
-      console.log("Error: ", error);
-    } else { // If we we're able to load it
-    
-      parseJSON(body, function(error, json) {
-        if(error) {
-          console.log("Error: ", error);
-        } else { // JSON successfully read
+	/* Get products from API */
+	request("https://ine5646products.herokuapp.com/api/products", function(error, response, body) {
+		if(error) {
+			console.log("Error: ", error);
+		} else { // If we we're able to load it
 
-          /* Connect to the database */
-          var db = new sqlite3.Database('athena.db', (err) => {
-            if(err) {
-              return console.error(err.message);
-            } else {
-              /* Clean table */
-              db.run("DELETE FROM Products", function(error) {
-                if(error) {
-                  console.log(error.message);
-                }
-              });
+			parseJSON(body, function(error, json) {
+				if(error) {
+					console.log("Error: ", error);
+				} else { // JSON successfully read
 
-              /* Register all products */
-              for(i = 0; i < json.length; i++) {
-                db.run("INSERT INTO Products VALUES (?, ?, ?)", json[i].id, json[i].name, json[i].enabled, function(error) {
-                  if(error) {
-                    console.log(error.message);
-                  }
-                });
-              }
-            }
-          });
-          db.close();
+					/* Connect to the database */
+					const db = new sqlite3.Database('athena.db', (err) => {
+						if(err) {
+							return console.error(err.message);
+						} else {
+							/* Clean table */
+							db.run("DELETE FROM Products", function(error) {
+								if(error) {
+									console.log(error.message);
+								}
+							});
 
-          res.status('202');
-          res.end("Accepted.");
-        }
-      });
-    }
-  });
+							/* Register all products */
+							for(let i = 0; i < json.length; i++) {
+								db.run("INSERT INTO Products VALUES (?, ?, ?)", json[i].id, json[i].name, json[i].enabled, function(error) {
+									if(error) {
+										console.log(error.message);
+									}
+								});
+							}
+						}
+					});
+					db.close();
+
+					res.status('202');
+					res.end("Accepted.");
+				}
+			});
+		}
+	});
 });
 
 module.exports = router;
