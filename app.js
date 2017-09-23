9bde8e9ac59b67af6aa46308433abcab66f4ad9b
Telmo Trooper <telmo.trooper@gmail.com>
2017-09-23 01:02:13 -0300

    Refactoring on app.js

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 /* Dependencies */
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const path = require('path');
 
 /* JavaScript files for each page */
-var index = require('./routes/index');
-var api = require('./routes/api');
-var api_bids = require('./routes/api_bids');
-var api_products = require('./routes/api_products');
-var api_products_in_biddings = require('./routes/api_products_in_biddings');
-var api_enabled_products = require('./routes/api_enabled_products');
-var time = require('./routes/time');
-var refresh_products = require('./routes/refresh_products');
+const index = require('./routes/index');
+const api = require('./routes/api');
+const api_bids = require('./routes/api_bids');
+const api_enabled_products = require('./routes/api_enabled_products');
+const api_products = require('./routes/api_products');
+const api_products_in_biddings = require('./routes/api_products_in_biddings');
+const refresh_products = require('./routes/refresh_products');
+const time = require('./routes/time');
 
 /* Initialize application */
-var app = express();
+const app = express();
 
 /* Set port and start listening */
 app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
 
 /* View engine setup */
@@ -49,20 +49,20 @@ app.use('/refresh_products', refresh_products);
 
 /* Catch 404 and forward to error handler */
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	let error = new Error('Not Found');
+	error.status = 404;
+	next(error);
 });
 
 /* Error handler */
 app.use(function(err, req, res, next) {
-  /* Set locals, only providing error in development */
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	/* Set locals, only providing error in development */
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  /* Render the error page */
-  res.status(err.status || 500);
-  res.render('error');
+	/* Render the error page */
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
