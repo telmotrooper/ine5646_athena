673736105bf2e25197a44b262be4d7401072b1c6
Telmo Trooper <telmo.trooper@gmail.com>
2017-10-14 18:16:21 -0300

    Progress on GH login

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,23 @@ const request = require('request');
 const parseJSON = require('json-parse-async');
 const router = express.Router();
 
+/* GitHub OAuth2 stuff */
+const client_id = "273dc5b8029e15a19161";
+const redirect_uri = "http://localhost:8000/login";
+const client_secret = "307d1a4298a32ed553ad074d79f182de39516ffc";
+
 router.get('/', function(req, res, next) {
-	console.log(req.query.code);
+	if(req.query.code !== undefined) {
+		console.log("Code: " + req.query.code);
+
+		request.post({
+			url: "https://github.com/login/oauth/access_token?client_id=" + client_id +
+			"&redirect_uri=" + redirect_uri + "&client_secret=" + client_secret +
+			"&code=" + req.query.code
+		}, function(error, response, body) {
+			console.log(response);
+		});
+	}
 
 	res.render('login', {
 		title: 'Login',
