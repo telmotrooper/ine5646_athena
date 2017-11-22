446f36014d54789a188fe525dca6a061d07f9388
Telmo Trooper <telmo.trooper@gmail.com>
2017-11-22 00:11:12 -0200

    Initial work on GraphQL

--- routes/graphql.js
+++ routes/graphql.js
@@ -4,18 +4,35 @@ const router = express.Router();
 
 let { buildSchema } = require("graphql");
 
+/* Reading from:
+   http://graphql.org/graphql-js/object-types/ */
+
 // Construct a schema, using GraphQL schema language
 let schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+	type Bid {
+		bidding: Int
+	}
+
+	type Query {
+		hello: String
+		bid: Bid
+	}
 `);
+
+class Bid {
+	bidding() {
+		return 10;
+	}
+}
 
 // The root provides a resolver function for each API endpoint
 let root  = {
   hello: () => {
     return "Hello, World!";
   },
+  bid: function() {
+	  return new Bid();
+  }
 };
 
 // Running the API endpoint
